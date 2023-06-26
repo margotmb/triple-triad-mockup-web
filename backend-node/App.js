4f11a74aea461acef9f4eea6d761921450cfa5e6
Margot M. Baisch <margot.mb@outlook.com>
2023-06-26 07:03:20 -0300

    Removendo dependência

--- backend-node/App.js
+++ backend-node/App.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
-const UserRouter = require("./routes/UserRoutes")
 
 // routes
 const cards = require('../routes/api/cards');
