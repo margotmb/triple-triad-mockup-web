c10865e4c13f866fce1953b2b50a89973e9cc076
Margot M. Baisch <margot.mb@outlook.com>
2023-06-28 15:03:52 -0300

    credentials

--- frontendreact/src/login.js
+++ frontendreact/src/login.js
@@ -14,6 +14,7 @@ function Login(){
 
         fetch('https://tripletriadapi.onrender.com/api/users/login', {
             method: 'POST',
+            credentials: 'include',
             headers: {
               'Content-Type': 'application/json'
             },
