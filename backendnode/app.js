653d70ff185abdc9c79847a2c569a890564f4bea
Margot M. Baisch <margot.mb@outlook.com>
2023-06-29 00:18:15 -0300

    cors 2

--- backendnode/app.js
+++ backendnode/app.js
@@ -28,7 +28,13 @@ mongoose.connect(
     },
     console.log("CONNECTED")
 );
-app.use(cors());
+// CORS middleware
+const allowCrossDomain = (req, res, next) => {
+    res.header(`Access-Control-Allow-Origin`, `https://tripletriadgame.onrender.com/`);
+    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+    res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+    next();
+  };
 //Read PUT/POST
 app.use(express.json({ extended: false }));
 
@@ -36,7 +42,7 @@ app.use(express.json({ extended: false }));
 
 
 module.exports = app;
-
+app.use(allowCrossDomain);
 // routes
 const cards = require('./routes/api/cards');
 const users = require('./routes/api/users');
