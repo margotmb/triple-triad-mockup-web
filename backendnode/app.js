383a8ad83dca8850e019d6c734c1da06fcafaccc
Margot M. Baisch <margot.mb@outlook.com>
2023-06-28 23:45:18 -0300

    cors test

--- backendnode/app.js
+++ backendnode/app.js
@@ -3,7 +3,7 @@ const sessions = require('express-session');
 const cookieParser = require("cookie-parser");
 const cors = require('cors');
 const app = express();
-app.use(cors({origin: 'https://www.tripletriadgame.onrender.com' , credentials: true}));
+app.use(cors({origin: "tripletriadgame.onrender.com"}));
 //Sessions
 const oneDay = 1000 * 60 * 60 * 24;
 app.use(sessions({
