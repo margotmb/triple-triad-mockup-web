3479ce44ba6926dd47a6aede0d2aad454d06d6c6
Margot M. Baisch <margot.mb@outlook.com>
2023-06-29 00:01:10 -0300

    fix login request

--- backendnode/app.js
+++ backendnode/app.js
@@ -3,7 +3,7 @@ const sessions = require('express-session');
 const cookieParser = require("cookie-parser");
 const cors = require('cors');
 const app = express();
-app.use(cors({origin: "tripletriadapi.onrender.com"}));
+
 //Sessions
 const oneDay = 1000 * 60 * 60 * 24;
 app.use(sessions({
