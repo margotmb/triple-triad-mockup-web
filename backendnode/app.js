d4da7f6446e82ab08bd61082adf2d51f658df51f
Margot M. Baisch <margot.mb@outlook.com>
2023-06-30 08:40:50 -0300

    cors test

--- backendnode/app.js
+++ backendnode/app.js
@@ -9,13 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 // cookie parser middleware
-app.use(cors({
-    origin: ['https://tripletriadgame.onrender.com',
-    'https://tripletriadgame.onrender.com/register', 
-    'https://tripletriadgame.onrender.com/home', 
-    'https://tripletriadgame.onrender.com/game', 
-    'https://tripletriadgame.onrender.com/account' ],
-    credentials: true,
+app.use(cors({credentials: true,
     }))
 //ConnectDB
 const mongoose = require("mongoose");
@@ -40,37 +34,13 @@ module.exports = app;
 // routes
 const cards = require('./routes/api/cards');
 const users = require('./routes/api/users');
-app.options('/api/cards', cors({
-    origin: ['https://tripletriadgame.onrender.com',
-    'https://tripletriadgame.onrender.com/register', 
-    'https://tripletriadgame.onrender.com/home', 
-    'https://tripletriadgame.onrender.com/game', 
-    'https://tripletriadgame.onrender.com/account' ],
-    credentials: true}));
+app.options('/api/cards');
 
-app.use('/api/cards', cards, cors({
-    origin: ['https://tripletriadgame.onrender.com',
-    'https://tripletriadgame.onrender.com/register', 
-    'https://tripletriadgame.onrender.com/home',
-     'https://tripletriadgame.onrender.com/game', 
-     'https://tripletriadgame.onrender.com/account' ],
-     credentials: true}));
+app.use('/api/cards', cards);
 
-app.options('/api/users', cors({
-    origin: ['https://tripletriadgame.onrender.com',
-    'https://tripletriadgame.onrender.com/register', 
-    'https://tripletriadgame.onrender.com/home', 
-    'https://tripletriadgame.onrender.com/game', 
-    'https://tripletriadgame.onrender.com/account' ],
-    credentials: true}));
+app.options('/api/users');
 
-app.use('/api/users', users, cors({
-    origin: ['https://tripletriadgame.onrender.com',
-    'https://tripletriadgame.onrender.com/register', 
-    'https://tripletriadgame.onrender.com/home', 
-    'https://tripletriadgame.onrender.com/game', 
-    'https://tripletriadgame.onrender.com/account' ],
-    credentials: true}));
+app.use('/api/users', users);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
