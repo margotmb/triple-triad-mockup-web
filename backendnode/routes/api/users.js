7310758f69dec42835ed65545976ade7ce51e125
Margot M. Baisch <margot.mb@outlook.com>
2023-06-29 22:40:26 -0300

    fix jwt

--- backendnode/routes/api/users.js
+++ backendnode/routes/api/users.js
@@ -123,7 +123,7 @@ router.post('/auth', (req, res) => {
 		// Note that we are passing the key in this method as well. This method will throw an error
 		// if the token is invalid (if it has expired according to the expiry time we set on sign in),
 		// or if the signature does not match
-		payload = jwt.verify(token, jwtKey)
+		payload = jwt.verify(token, "shhh")
 	} catch (e) {
 		if (e instanceof jwt.JsonWebTokenError) {
 			// if the error thrown is because the JWT is unauthorized, return a 401 error
