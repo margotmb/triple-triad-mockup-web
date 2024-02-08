74b6f6e33b1ee13b11c30d4cf89e28350aa8e18f
Margot M. Baisch <73503491+margotmb@users.noreply.github.com>
2024-02-08 17:17:13 -0300

    Update app.js

--- backendnode/app.js
+++ backendnode/app.js
@@ -20,7 +20,7 @@ app.use(cors({
 //ConnectDB
 const mongoose = require("mongoose");
 mongoose.connect(
-    process.env.MONGODB_URI = "mongodb+srv://margotmb:yWJNohfPTApjKxkO@tripletriad.6dyldmh.mongodb.net/", 
+    process.env.MONGODB_URI = "", 
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
