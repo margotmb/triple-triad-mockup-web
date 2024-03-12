aa628e4d06ebb2a4a466d5155a0d7345eae9c9ca
Margot M. Baisch <margot.mb@outlook.com>
2024-03-12 20:52:27 -0300

    Clean up

--- backendnode/app.js
+++ backendnode/app.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const cors = require('cors');
-// const cookieParser = require("cookie-parser")
 const app = express();
 
-// app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
