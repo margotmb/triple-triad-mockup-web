a33c772f89c0e867391405dc5202dbc4457ede33
Margot M. Baisch <margot.mb@outlook.com>
2024-03-12 23:20:11 -0300

    App Cleanup and Express Session

--- backendnode/app.js
+++ backendnode/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require('cors');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const app = express();
 
 app.use(express.json());
@@ -17,6 +19,19 @@ mongoose.connect(
     },
     console.log("CONNECTED")
 );
+// Sessions Middleware Configuration
+const store = new MongoDBStore({
+    uri: '',
+    collection: 'sessions'
+  });
+
+app.use(session({
+    secret: '',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+    store: store
+}));
 
 //PORT SET
 var port = 5000;
@@ -25,11 +40,13 @@ module.exports = app;
 // routes
 const cards = require('./routes/cards');
 const users = require('./routes/users');
-//app.options('/cards');
+const sessions = require('./routes/sessions');
 
 app.use('/cards', cards);
 
 app.use('/users', users);
+
+app.use('/sessions', sessions);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
