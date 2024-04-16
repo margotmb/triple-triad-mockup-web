76a9a9f7c4bfc072cbeb977c121f9c99bf417f25
Margot M. Baisch <73503491+margotmb@users.noreply.github.com>
2024-04-16 13:22:47 +0000

    reviewing code

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,94 +1,89 @@
-import React, { useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/login.css";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useNavigate } from "react-router-dom";
-import Cookies from 'universal-cookie';
+import Cookies from "universal-cookie";
 
-
-function Login(){
-    const cookies = new Cookies();
-    const navigate = useNavigate()
-    useEffect(() => {
-        if (cookies.get('token') != undefined){
-            navigate("/home");
-        }
-    });
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const handleSubmit = async e => {
-
-        fetch('', {
-            method: 'POST',
-            mode: 'cors',
-            credentials: "include",
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({"email": email, "password":password})
-          })
-            .then(data => data.json())
-            .then(data =>{
-                if (data.result == false){
-                    alert("Email ou Password errado")
-                }
-                else{
-                    
-                    cookies.set('token', data, {sameSite: 'lax'});
-                    console.log(cookies.get('token'));
-                    console.log("LOGGED");
-                        
-                    navigate("/home")
-
-                }
-            })
-            
-        e.preventDefault();
+// Refactor if universal-cookie is needed
+// Express already has session middleware
+function Login() {
+  const cookies = new Cookies();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (cookies.get("token") != undefined) {
+      navigate("/home");
     }
-    return(
-        <React.Fragment>
-            <img className="title_login" src="https://i.imgur.com/T3ybYPx.png" ></img>
-            <div className="Auth-form-container">
-                <form className="Auth-form" onSubmit={handleSubmit}>
-                    <div className="Auth-form-content">
-                    <h3 className="Auth-form-title">Sign In</h3>
-                    <div className="form-group mt-3">
-                        <label>Email address</label>
-                        <input
-                        type="email"
-                        className="form-control mt-1"
-                        placeholder="Enter email"
-                        onChange={e => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group mt-3">
-                        <label>Password</label>
-                        <input
-                        type="password"
-                        className="form-control mt-1"
-                        placeholder="Enter password"
-                        onChange={e => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <div className="d-grid gap-2 mt-3">
-                        <button type="submit" className="btn btn-primary" >
-                        Log In
-                        </button>
-                    </div>
-                    <div className="d-grid gap-2 mt-3">
-                        <button onClick={() => navigate("/register")} className="btn btn-primary" >
-                        Create New Account
-                        </button>
-                    </div>
-                    </div>
-                </form>
+  });
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const handleSubmit = async (e) => {
+    fetch("", {
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email, password: password }),
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        if (data.result == false) {
+          alert("Email ou Password errado");
+        } else {
+          cookies.set("token", data, { sameSite: "lax" });
+          console.log(cookies.get("token"));
+          console.log("LOGGED");
+
+          navigate("/home");
+        }
+      });
+
+    e.preventDefault();
+  };
+  return (
+    <React.Fragment>
+      <img className="title_login" src="https://i.imgur.com/T3ybYPx.png"></img>
+      <div className="Auth-form-container">
+        <form className="Auth-form" onSubmit={handleSubmit}>
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Sign In</h3>
+            <div className="form-group mt-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="Enter email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
-        </React.Fragment>
-        
-        
-    )
-
-
-    
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Enter password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button type="submit" className="btn btn-primary">
+                Log In
+              </button>
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button
+                onClick={() => navigate("/register")}
+                className="btn btn-primary"
+              >
+                Create New Account
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </React.Fragment>
+  );
 }
 
-export default Login
+export default Login;
