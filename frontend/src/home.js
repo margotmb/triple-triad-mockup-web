f6d299600f4251177f9e5dce5873c2e63616deb9
Margot M. Baisch <73503491+margotmb@users.noreply.github.com>
2024-04-16 13:24:04 +0000

    reviewing code

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -1,42 +1,47 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./styles/home.css";
 import Navigation from "./navbar";
-import Cookies from 'universal-cookie';
+import Cookies from "universal-cookie";
 import { useNavigate } from "react-router-dom";
 
-function Home(){
-    const cookies = new Cookies();
-    const navigate = useNavigate()
-    var token = cookies.get("token");
-    const [name, setName] = useState(null)
-    const [email, setEmail] = useState(null)
+// Review if universal-cookie is needed
+// Express has session middleware
+function Home() {
+  const cookies = new Cookies();
+  const navigate = useNavigate();
+  var token = cookies.get("token");
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
 
-    fetch('https://tripletriadapi.onrender.com/api/users/auth',{
-        method: 'POST',
-        mode: 'cors',
-        credentials: "include",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify ({"token" : token})
+  fetch("/api/users/auth", {
+    method: "POST",
+    mode: "cors",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ token: token }),
+  })
+    .then((user) => user.json())
+    .then((user) => {
+      setName(user.name);
+      setEmail(user.email);
+    });
 
-      })
-      .then(user => user.json())
-      .then(user => {
-        setName(user.name)
-        setEmail(user.email)
-      })
-    
-
-    return(
-        <React.Fragment>
-            <Navigation email={email}/>
-            <div className="main-window">
-                <h2>Welcome, <u>{name}</u></h2>
-                <img className="title_login" src="https://i.imgur.com/T3ybYPx.png" ></img>
-            </div>
-        </React.Fragment>  
-    )
+  return (
+    <React.Fragment>
+      <Navigation email={email} />
+      <div className="main-window">
+        <h2>
+          Welcome, <u>{name}</u>
+        </h2>
+        <img
+          className="title_login"
+          src="https://i.imgur.com/T3ybYPx.png"
+        ></img>
+      </div>
+    </React.Fragment>
+  );
 }
 
-export default Home
+export default Home;
