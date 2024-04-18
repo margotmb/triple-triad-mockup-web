b7709c34758095266abadf4a7ff6a866da878b93
Margot M. Baisch <73503491+margotmb@users.noreply.github.com>
2024-04-18 01:12:22 +0000

    fetch requests for login

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, {useRef } from "react";
 import "./styles/login.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useNavigate } from "react-router-dom";
@@ -6,19 +6,35 @@ import { useNavigate } from "react-router-dom";
 
 function Login() {
   const navigate = useNavigate();
-  /* Create UseEffect to deal with logged user
-  useEffect(() => {
-    if (cookies.get("token") != undefined) {
-      navigate("/home");
+  const api_url_auth = process.env.REACT_APP_API_URL + "/sessions/auth"
+  // Checks if user is already logged in, then moves to /home if yes
+  const fetchPromise = fetch(api_url_auth, {
+    method: "GET",
+    mode: "cors",
+    credentials: "include"})
+  
+  fetchPromise.then(response => {
+    if (response.status === "200"){
+      const jsonPromise = response.json();
+      jsonPromise.then(data => {
+        console.log("Successful request, parsed json body", data);
+        navigate("/home")
+      }).catch(error => {
+        console.log("Successful request, Could not parse body as json", error);
+      })
     }
-  });
-  */
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const api_url = process.env.REACT_APP_API_URL + "/login"
+    else{
+      console.log("Request not successful")
+    }
+  })
+
+  // Login Form
+  const email = useRef("")
+  const password = useRef("")
+  const api_url_login = process.env.REACT_APP_API_URL + "/login"
 
   const handleSubmit = async (e) => {
-    fetch(api_url, {
+    fetch(api_url_login, {
       method: "POST",
       mode: "cors",
       credentials: "include",
@@ -26,22 +42,11 @@ function Login() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ email: email, password: password }),
-    })
-      .then((data) => data.json())
-      .then((data) => {
-        if (data.result == false) {
-          alert("Email ou Password errado");
-        } else {
-          cookies.set("token", data, { sameSite: "lax" });
-          console.log(cookies.get("token"));
-          console.log("LOGGED");
-
-          navigate("/home");
-        }
-      });
-
-    e.preventDefault();
-  };
+    }).then((data) => data.json()).then((data) => {
+          console.log(data)
+        })
+        e.preventDefault();
+      };
   return (
     <React.Fragment>
       <img className="title_login" src="https://i.imgur.com/T3ybYPx.png"></img>
@@ -55,7 +60,7 @@ function Login() {
                 type="email"
                 className="form-control mt-1"
                 placeholder="Enter email"
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => email.current = e.target.value}
               />
             </div>
             <div className="form-group mt-3">
@@ -64,11 +69,11 @@ function Login() {
                 type="password"
                 className="form-control mt-1"
                 placeholder="Enter password"
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => password.current = e.target.value}
               />
             </div>
             <div className="d-grid gap-2 mt-3">
-              <button type="submit" className="btn btn-primary">
+              <button type="submit" className="btn btn-primary" onClick={() => {}}>
                 Log In
               </button>
             </div>
