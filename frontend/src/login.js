107f475f95988635bf93b6c2b03e52a0b86ef40a
Margot M. Baisch <73503491+margotmb@users.noreply.github.com>
2024-04-19 15:39:04 +0000

    update login.js

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,52 +1,73 @@
-import React, {useRef } from "react";
+import React, { useRef } from "react";
 import "./styles/login.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useNavigate } from "react-router-dom";
 
-
 function Login() {
   const navigate = useNavigate();
-  const api_url_auth = process.env.REACT_APP_API_URL + "/sessions/auth"
+  const api_url_auth = process.env.REACT_APP_API_URL + "/sessions/auth";
   // Checks if user is already logged in, then moves to /home if yes
   const fetchPromise = fetch(api_url_auth, {
     method: "GET",
     mode: "cors",
-    credentials: "include"})
-  
-  fetchPromise.then(response => {
-    if (response.status === "200"){
+    credentials: "include",
+  });
+
+  fetchPromise.then((response) => {
+    if (response.status === "200") {
       const jsonPromise = response.json();
-      jsonPromise.then(data => {
-        console.log("Successful request, parsed json body", data);
-        navigate("/home")
-      }).catch(error => {
-        console.log("Successful request, Could not parse body as json", error);
-      })
+      jsonPromise
+        .then((data) => {
+          console.log("Successful request, parsed json body", data);
+          navigate("/home");
+        })
+        .catch((error) => {
+          console.log(
+            "Successful request, Could not parse body as json",
+            error,
+          );
+        });
+    } else {
+      console.log("Request not successful");
     }
-    else{
-      console.log("Request not successful")
-    }
-  })
+  });
 
   // Login Form
-  const email = useRef("")
-  const password = useRef("")
-  const api_url_login = process.env.REACT_APP_API_URL + "/login"
+  const email = useRef("");
+  const password = useRef("");
+  const api_url_login = process.env.REACT_APP_API_URL + "/sessions/login";
 
   const handleSubmit = async (e) => {
-    fetch(api_url_login, {
-      method: "POST",
-      mode: "cors",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email: email, password: password }),
-    }).then((data) => data.json()).then((data) => {
-          console.log(data)
-        })
-        e.preventDefault();
-      };
+    if (email != "" && password != "") {
+      fetch(api_url_login, {
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: email.current, password: password.current }),
+      })
+        .then((data) => data.json())
+        .then((user) => {
+          if (user.response == "ok") {
+            
+            fetch(process.env.REACT_APP_API_URL + "/cards", {
+              method:"GET",
+              mode:"cors",
+              credentials:"include"
+              
+            })
+            navigate("/home");
+          } else {
+            alert("Incorrect Email or Password / User does not exist");
+          }
+        });
+      e.preventDefault();
+    } else {
+      console.log("email or password empty");
+    }
+  };
   return (
     <React.Fragment>
       <img className="title_login" src="https://i.imgur.com/T3ybYPx.png"></img>
@@ -60,7 +81,7 @@ function Login() {
                 type="email"
                 className="form-control mt-1"
                 placeholder="Enter email"
-                onChange={(e) => email.current = e.target.value}
+                onChange={(e) => (email.current = e.target.value)}
               />
             </div>
             <div className="form-group mt-3">
@@ -69,11 +90,15 @@ function Login() {
                 type="password"
                 className="form-control mt-1"
                 placeholder="Enter password"
-                onChange={(e) => password.current = e.target.value}
+                onChange={(e) => (password.current = e.target.value)}
               />
             </div>
             <div className="d-grid gap-2 mt-3">
-              <button type="submit" className="btn btn-primary" onClick={() => {}}>
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={() => {}}
+              >
                 Log In
               </button>
             </div>
