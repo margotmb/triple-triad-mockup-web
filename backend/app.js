2d2a04c0f6b55a27fe00de79b352b03f41118a54
Margot M. Baisch <73503491+margotmb@users.noreply.github.com>
2024-04-19 15:34:31 +0000

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,74 +1,61 @@
 //Imports
 const express = require("express");
-const cors = require('cors');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const dotenv = require('dotenv')
+const cors = require("cors");
+const session = require("express-session");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
-
-//Set Origins
-var corsOptions = {
-  origin: process.env.CORSORIGIN,
-  credentials: true };
-dotenv.config()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions))
 
-//ConnectDB
+//CORS
+var corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+//ConnectDB mongoose
 const mongoose = require("mongoose");
 mongoose.connect(
-    process.env.MONGODB_URI, 
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        dbName: 'tripletriad',
-    },
-    console.log("CONNECTED")
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "tripletriad",
+  },
+  console.log("CONNECTED"),
 );
 
-// Sessions Middleware Configuration
-const store = new MongoDBStore({
-    uri: process.env.MONGODB_URI,
-    databaseName: 'tripletriad',
-    collection: 'sessions'
-  });
-
-app.use(session({
+// Express-Session Config
+app.use(
+  session({
     secret: process.env.SESSIONSECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: true },
-    store: store
-}));
+    cookie: { httpOnly: true, secure: false },
+    store: store,
+  }),
+);
 
 // PORT SET
 var port = process.env.PORT;
 module.exports = app;
 
 // Routes w/ Express Routers
-const cards = require('./routes/cards');
-const users = require('./routes/users');
-const sessions = require('./routes/sessions');
+const cards = require("./routes/cards");
+const users = require("./routes/users");
+const sessions = require("./routes/sessions");
 
-app.use('/cards', cards);
+app.use("/cards", cards);
+app.use("/users", users);
+app.use("/sessions", sessions);
 
-app.use('/users', users);
-
-app.use('/sessions', sessions);
-
-app.get('/', (req, res) => {
-    res.send('HomeRoute')
-  })
+app.get("/", (req, res) => {
+  res.send("HomeRoute");
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-  })
-
-
-
-
-
-
-
+  console.log(`Listening on port ${port}`);
+});
